a2fe735d3c49c2aa1f3aa7aaefd4cb32acd66417
huyycp <baohuyvongoc1@gmail.com>
2023-11-03 10:59:25 +0700

    threadpool, optimize IOStreamHelper,multi file type download

--- src/FileDownloadBooster.java
+++ src/FileDownloadBooster.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 public class FileDownloadBooster {
     public static void main(String[] args) {
@@ -6,12 +8,27 @@ public class FileDownloadBooster {
         String lmsURL = "http://lms.dut.udn.vn/pluginfile.php/188023/mod_resource/content/1/Modern%20Operating%20Systems%204th%20Edition--Andrew%20Tanenbaum.pdf"; // code 303
         String httpsURL = "https://www.k12blueprint.com/sites/default/files/Learning-Management-System-Guide.pdf"; // pass
         String tmpURL = "https://cuuduongthancong.com/dlf/2334741/cau-truc-du-lieu-va-giai-thuat/pham-the-bao/slides---cay.pdf";
-        String imgURL = "https://cuuduongthancong.com/dlf/102433/he-dieu-hanh/2012---2013-2.jpg";
-        String localhostURL = "http://localhost/MVCExample/index.html";
+        String imgURL = "https://sample-videos.com/img/Sample-jpg-image-30mb.jpg"; // pass
+        String docURL = "https://sample-videos.com/doc/Sample-doc-file-5000kb.doc"; // pass
+        String pptURL = "https://sample-videos.com/ppt/Sample-PPT-File-1000kb.ppt"; // pass
+        String zipURL = "https://sample-videos.com/zip/100mb.zip"; // pass (12s / 10s edge)
+        String xlsURL = "https://sample-videos.com/xls/Sample-Spreadsheet-50000-rows.xls"; // pass
+        String csvURL = "https://sample-videos.com/csv/Sample-Spreadsheet-500000-rows.csv"; // pass
+        String mp4URL = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_30mb.mp4"; // pass
+        String flvURL = "https://sample-videos.com/video123/flv/720/big_buck_bunny_720p_30mb.flv"; // pass
+        String exeURL = "https://get.videolan.org/vlc/3.0.20/win64/vlc-3.0.20-win64.exe"; // pass
+        String pngURL = "https://sample-videos.com/img/Sample-png-image-30mb.png"; // pass
+        String gifURL = "https://sample-videos.com/gif/2.gif"; // pass
+        String mp3URL = "https://sample-videos.com/audio/mp3/wave.mp3"; // pass
+        String zipURLpro = "http://212.183.159.230/1GB.zip"; // pass (6mb/s 5p)
+
         String savePath = "D:/PBL_Storage/";
         try {
-            FragmentDownloadManager manager  = new FragmentDownloadManager(httpsURL);
-            manager.downloadFile(savePath);
+            FragmentDownloadManager manager  = new FragmentDownloadManager();
+            manager.downloadFile(pngURL, savePath);
+
+        } catch (MalformedURLException | URISyntaxException e) {
+            System.out.println("Invalid URL");
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/FragmentDownloadManager.java
+++ src/FragmentDownloadManager.java
@@ -1,30 +1,40 @@
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.Socket;
-import java.net.URL;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.*;
 
 public class FragmentDownloadManager {
-    URL url; // Unhandled URL
-    final static int numThreads = 10;
+    private final int numThreads = 10;
+    private final int corePoolSize = numThreads; // minimum number of threads run simultaneously
+    private final int maximumPoolSize = numThreads * 3; // maximum number of threads run simultaneously
+    private final long keepAliveTime = 10L; // time to live while not running (threads = maximumPoolSize - coorePoolSize)
+    private final TimeUnit unit = TimeUnit.SECONDS;
+    private final int queueSize = numThreads * 3;
+    private final ArrayBlockingQueue<Runnable> workQueue; // queue of runnable tasks
+    private final RejectedExecutionHandler handler; // handle rejected execution
+    private ThreadPoolExecutor executor; // multithread manager
 
-    public FragmentDownloadManager(String urlStr) throws MalformedURLException {
-        this.url = new URL(urlStr);
+    public FragmentDownloadManager() throws MalformedURLException, URISyntaxException {
+        this.workQueue = new ArrayBlockingQueue<>(queueSize);
+        this.handler = new ThreadPoolExecutor.CallerRunsPolicy();
+        this.executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
     }
-    public void HandleRedirectURL() {
+    public URL HandleRedirectURL(String strURL) throws URISyntaxException, MalformedURLException {
+        URL url = new URI(strURL).toURL();
         System.out.println("Handling redirect URL...");
-        String responseCode = ""; // Response code
+        String responseCode = "";
         do {
             try (Socket socket = url.getProtocol().equals("https") ? SSLSocketFactory.getDefault().createSocket(url.getHost(), 443) : new Socket(url.getHost(), 80);
                  BufferedInputStream inputStream = new BufferedInputStream(socket.getInputStream());
                  BufferedOutputStream outputStream = new BufferedOutputStream(socket.getOutputStream())) {
 
                 // Create a http HEAD request
-
                 String request = "HEAD " + url.getPath() + " HTTP/1.1\r\n" +
                         "Host: " + url.getHost() + "\r\n\r\n";
+
                 // Send request to server
                 IOStreamHelper.sendRequest(outputStream, request);
 
@@ -40,28 +50,31 @@ public class FragmentDownloadManager {
                 // Handle redirect
                 String statusLine = headers.get("status");
                 responseCode = statusLine.substring(statusLine.indexOf(" ") + 1, statusLine.indexOf(" ") + 4);
-                if(responseCode.equals("200")) { break;}
-                else if(responseCode.startsWith("3")) {
+                if(responseCode.equals("200")) {
+                    break;
+                } else if(responseCode.startsWith("3")) {
                     String location = headers.get("location");
-                    url = new URL(location);
+                    url = new URI(location).toURL();
                 }
             } catch (IOException exception) {
                 System.out.println(exception.getMessage());
                 System.out.println("Handle redirect failed");
             }
         } while(true);
+        return url;
     }
 
-    public void downloadFile(String savePath) throws IOException {
+    public void downloadFile(String strURL, String savePath) throws IOException, URISyntaxException {
         // Handle redirect URL
-        HandleRedirectURL();
-        System.out.println("Final URL: " + url);
+        URL url =  HandleRedirectURL(strURL);
+
+        System.out.println("\nFinal URL: " + url);
 
         // Handle string URL
-        String protocol = url.getProtocol(); // Get prototcol
-        String host = url.getHost(); // Get host
-        String path = url.getPath(); // Get path
-        String filename = path.substring(path.lastIndexOf('/') + 1); // Get file name
+        String protocol = url.getProtocol();
+        String host = url.getHost();
+        String path = url.getPath();
+        String filename = path.substring(path.lastIndexOf('/') + 1);
         savePath += filename; // Add file name to directory
 
         // Create a socket connection
@@ -79,33 +92,44 @@ public class FragmentDownloadManager {
             // Read response from HEAD request
             Map<String, String> headers = IOStreamHelper.receiveHeader(inputStream);
 
+            // Close the connection
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+
+            // Calculate download range
             System.out.println("\nDownloading file...");
-            CountDownLatch latch = new CountDownLatch(numThreads);
             long fileSize = Long.parseLong(headers.get("content-length"));
             long partSize = fileSize / numThreads;
+
+            // Multipart download
+            List<Future<Long>> futures = new ArrayList<>();
             for (int i = 0; i < numThreads; i++) {
                 // Range to download
                 long startByte = i * partSize;
                 long endByte = (i == numThreads - 1) ? fileSize - 1 : startByte + partSize - 1;
 
                 // Download by range
+                FragmentDownloader fragmentDownloader = new FragmentDownloader(i + 1, url, savePath, startByte, endByte);
+                Future<Long> future = executor.submit(fragmentDownloader);
+                futures.add(future);
+            }
+
+            // Wait for all threads to finish
+            long totalBytesRead = 0;
+            for(Future<Long> future : futures) {
                 try {
-                    FragmentDownloader fragmentDownloader = new FragmentDownloader(i + 1, url, savePath, startByte, endByte, latch);
-                    fragmentDownloader.startDownload();
-                } catch (InterruptedException e) {
-                    System.out.println("Thread " + (i + 1) + " is interrupted");
+                    totalBytesRead += future.get();
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
 
-            try {
-                // Wait for all threads to finish
-                latch.await();
-                System.out.println("File has been successfully downloaded.");
-            } catch (InterruptedException e) {
-                System.out.println("Threads interrupted");
-            }
+            System.out.println("Total bytes read: " + totalBytesRead);
+            System.out.println("File has been successfully downloaded.\n\n");
+//            executor.shutdown();
         } catch (IOException exception) {
             System.out.println("Connection closed");
-        } 
+        }
     }
 }
